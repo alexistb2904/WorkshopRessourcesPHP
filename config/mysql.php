070e03d0ac8bb8b5d931cdb377aea5fb2638c087
alexistb2904 <alexistb2904@gmail.com>
2023-06-19 11:38:39 +0200

    Update mysql.php

--- config/mysql.php
+++ config/mysql.php
@@ -1,25 +1,21 @@
 <?php
 
-error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 
     $mysqlHost = $_ENV['MYSQL_HOST'];
     $mysqlUser = $_ENV['MYSQL_USERNAME'];
     $mysqlPassword = $_ENV['MYSQL_PASSWORD'];
     $mysqlName = $_ENV['MYSQL_DATABASE'];
     $mysqlPort = 3306;
-
-    const MYSQL_HOST = $mysqlHost;
-    const MYSQL_USER = $mysqlUser;
-    const MYSQL_PASSWORD = $mysqlPassword;
-    const MYSQL_NAME = $mysqlName;
-    const MYSQL_PORT = $mysqlPort;
-
+    
 try {
     $mysqlClient = new PDO(
-        sprintf('mysql:host=%s;dbname=%s;port=%s', MYSQL_HOST, MYSQL_NAME, MYSQL_PORT),
-        MYSQL_USER,
-        MYSQL_PASSWORD
+        sprintf('mysql:host=%s;dbname=%s;port=%s', $mysqlHost, $mysqlName, $mysqlPort),
+        $mysqlUser,
+        $mysqlPassword
     );
     $mysqlClient->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(Exception $exception) {
