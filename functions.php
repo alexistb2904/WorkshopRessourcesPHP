a3639a5c53e32a0813bdfecd75403d24fc6db9d8
alexistb2904 <alexistb2904@gmail.com>
2023-06-19 11:25:59 +0200

    Test 2

--- functions.php
+++ functions.php
@@ -27,10 +27,10 @@ function get_car(array $category) : array
 
 function is_admin($email): bool
 {
-    $host = "54.37.204.19";
-    $username = "u79805_RH6t4MFKuh";
-    $password = "h2w+Lx5w.RPvqU+xB0YeiuTu";
-    $database = "s79805_WorkshopRessources";
+    $host = $_ENV['MYSQL_HOST'];
+    $username = $_ENV['MYSQL_USERNAME'];
+    $password = $_ENV['MYSQL_PASSWORD'];
+    $database = $_ENV['MYSQL_DATABASE'];
 
     $mysqlClient = mysqli_connect($host, $username, $password, $database);
 
