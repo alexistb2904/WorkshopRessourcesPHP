91eae598dcf75c65b40af333c8152d86e7e3bbb5
alexistb2904 <alexistb2904@gmail.com>
2023-06-20 20:46:42 +0200

    Panel

--- user/home.php
+++ user/home.php
@@ -137,6 +137,7 @@ $decals_c_p = $decals_c_pFetch->fetchAll();
                 </div>
             </details>
         <?php endforeach; ?>
+        <a class="nav-link" href="<?php echo($rootUrl). 'user/home.php'; ?>">Panel User</a>
     </main>
 <?php } else { ?>
 
