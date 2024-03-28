65e164d227fdb5ee24e6a3c0d75fcee458064f94
alexistb2904 <alexistb2904@gmail.com>
2024-03-28 14:54:22 +0100

    vendor autoload

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '767278036c27a688be11444004fa3ca97434e99d',
+        'reference' => '680f98e6e200b2f5970d0bb219365cb93896e2f8',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '767278036c27a688be11444004fa3ca97434e99d',
+            'reference' => '680f98e6e200b2f5970d0bb219365cb93896e2f8',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
