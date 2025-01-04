b382bcb18555fc471f63f77b1c88fe85da017049
alexistb2904 <alexistb2904@gmail.com>
2025-01-04 22:02:10 +0100

    Fix Error imgur

--- panel/admin/add.php
+++ panel/admin/add.php
@@ -179,30 +179,31 @@ if (isset($postData['send'])) {
     }
 }
 
-function uploadImage($PhotoImgur) {
+function uploadImage($img) {
+        $PhotoImgur = $img;
         $curl = curl_init();
 
-        curl_setopt_array(
-            $curl,
-            array(
-                CURLOPT_URL => 'https://api.imgur.com/3/image',
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'POST',
-                CURLOPT_POSTFIELDS => array('image' => base64_encode(file_get_contents($PhotoImgur['tmp_name'][0]))),
-                CURLOPT_HTTPHEADER => array(
-                    'Authorization: Client-ID f040db64c4332f9'
-                ),
-            )
-        );
+                        curl_setopt_array(
+                            $curl,
+                            array(
+                                CURLOPT_URL => 'https://api.imgur.com/3/image',
+                                CURLOPT_RETURNTRANSFER => true,
+                                CURLOPT_ENCODING => '',
+                                CURLOPT_MAXREDIRS => 10,
+                                CURLOPT_TIMEOUT => 0,
+                                CURLOPT_FOLLOWLOCATION => true,
+                                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                                CURLOPT_CUSTOMREQUEST => 'POST',
+                                CURLOPT_POSTFIELDS => array('image' => base64_encode(file_get_contents($PhotoImgur['tmp_name']))),
+                                CURLOPT_HTTPHEADER => array(
+                                    'Authorization: Client-ID f040db64c4332f9'
+                                ),
+                            )
+                        );
 
-        $response = curl_exec($curl);
-        curl_close($curl);
-        $response = json_decode($response, true);
+                        $response = curl_exec($curl);
+                        curl_close($curl);
+                        $response = json_decode($response, true);
         var_dump($response);
         if ($response['success'] === true) {
             $photo = $response['data']['link'];
